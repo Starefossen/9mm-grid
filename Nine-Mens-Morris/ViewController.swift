26134ae40861035b17c5e43e800c06d550fcc0ea
Hans Kristian Flaatten <hans.kristian.flaatten@turistforeningen.no>
2014-09-10 21:00:03 +0200

    Kake

--- Nine-Mens-Morris/ViewController.swift
+++ Nine-Mens-Morris/ViewController.swift
@@ -10,6 +10,10 @@ class ViewController: UIViewController {
     var location = CGPoint(x: 0, y: 0)
     var imageView = UIImageView()
     var pointView = UIImageView()
+    var points = [(UIImageView, Int)]()
+    var game = Game()
+    var state_init = true
+    var player1 = true
     
     override func viewDidLoad() {
         super.viewDidLoad();
@@ -22,23 +26,51 @@ class ViewController: UIViewController {
         imageView.image = drawCustomImage(imageSize)
         
         // Point
+        
+        var grid = [
+            [(1, 0, 0), (2, 0, 150), (3, 0, 300)],
+            [(9, 50, 50), (10, 50, 150), (11, 50, 250)],
+            [(17, 100, 100), (18, 100, 150), (19, 100, 200)],
+            [(8, 150, 0), (16, 150, 50), (24, 150, 100), (20, 150, 200), (12, 150, 250), (4, 150, 300)],
+            [(23, 200, 100), (22, 200, 150), (21, 200, 200)],
+            [(15, 250, 50), (14, 250, 150), (13, 250, 250)],
+            [(7, 300, 0), (6, 300, 150), (5, 300, 300)]
+        ]
+        
         var pointSize = CGSize(width: 20, height: 20)
-        pointView = UIImageView(frame: CGRect(origin: CGPoint(x: 2*50+10-10, y: 3*50+35-10), size: pointSize))
-        pointView.userInteractionEnabled = true
-        self.view.addSubview(pointView)
-        pointView.image = drawPointImage(pointSize)
+        for row in grid {
+            for cel in row {
+                // cell.0 id
+                // cell.1 x
+                // coll.2 y
+                
+                pointView = UIImageView(frame: CGRect(origin: CGPoint(x: cel.1, y: cel.2+25), size: pointSize))
+                pointView.userInteractionEnabled = true
+                self.view.addSubview(pointView)
+                pointView.image = drawPointImage(pointSize)
+                //pointView.image.description = cel.0 as String!
+                
+                points.append((pointView, cel.0))
+            }
+        }
     }
     
     override func touchesBegan(touches: NSSet!, withEvent event: UIEvent!) {
         let touch:UITouch  = touches.anyObject() as UITouch
         location = touch.locationInView(self.view)
         
-        if (touch.view == imageView) {
-            print("Grid was touched\n")
-        } else if (touch.view == pointView) {
-            print("Point was touched\n")
-        } else {
-            print("Nothing was touched\n")
+        print(location)
+        
+        for point in points {
+            if (touch.view == point.0) {
+                println("Whoho \(point.1)");
+                
+                if (state_init) {
+                    if (player1) {
+                        //point.0.image = drawPointImage(
+                    }
+                }
+            }
         }
     }
 }
